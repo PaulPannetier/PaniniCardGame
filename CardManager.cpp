531022c6cccd20dd8741a55051a598e7d88751c7
Léonard P <leonard.pannetier@laposte.net>
2023-01-28 19:58:08 +0100

    envoie de l'executable

--- CardManager.cpp
+++ CardManager.cpp
@@ -21,9 +21,9 @@ void CardsManager::CreateCards()
 {
 	this->gameCards = 
 	{
-		Card("Pavard", "Second poteau\n Pavard!", 5, 4, 3, CardType::striker, "Pavard"),
-		Card("Giroud", "Encore raté", 3, 3, 2, CardType::defender, "Giroud"),
-		Card("Lorris", "Ramener la coupe\n à la maison", 5, 3, 2, CardType::goalkeeper, "Lioris"),
+		Card("Pavard", "Second poteau\n Pavard!", 5, 4, 4, CardType::striker, "Pavard"),
+		Card("Giroud", "", 2, 2, 2, CardType::defender, "Giroud"),
+		Card("Lorris", "Ramener la coupe\n à la maison", 1, 5, 3, CardType::goalkeeper, "Lioris"),
 		Card("Ronaldo", "Gagne +1 d'attaque\n au début de chaque\n tour", 1, 5, 5, CardType::striker, "Ronaldo", false, new IncreaseStatAtTurn(1,0))
 	};
 }
